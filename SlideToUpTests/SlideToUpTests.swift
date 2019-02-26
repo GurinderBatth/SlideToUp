9bd34638a6a35dcc7ce70409940e447f8f5ab6a9
Apple <apple@apples-imac.local>
2019-02-26 22:27:30 +0530

    Initial Commit

---
+++ SlideToUpTests/SlideToUpTests.swift
@@ -0,0 +1,34 @@
+//
+//  SlideToUpTests.swift
+//  SlideToUpTests
+//
+//  Created by Apple on 26/02/19.
+//  Copyright © 2019 Batth. All rights reserved.
+//
+
+import XCTest
+@testable import SlideToUp
+
+class SlideToUpTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
